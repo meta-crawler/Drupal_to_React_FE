208145b4c70bb6e934eb1a5ad72966cbde5bb712
meta-crawler <williamlei0906@gmail.com>
2022-11-17 20:31:31 -0800

    A skeleton of a dashboard system. This includes OAuth to Drupal, login screen which authorizes a user.

---
+++ src/App.js
@@ -0,0 +1,35 @@
+// project import
+import Routes from 'routes';
+import ThemeCustomization from 'themes';
+import ScrollTop from 'components/ScrollTop';
+import { Link as RouterLink, useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+import { getAuthClient } from './utils/auth';
+
+const auth = getAuthClient();
+
+// ==============================|| APP - THEME, ROUTER, LOCAL  ||============================== //
+
+const App = () => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        auth.isLoggedIn()
+            .then(() => {
+                // navigate('/dashboard/default');
+            })
+            .catch(() => {
+                navigate('/login');
+            });
+    }, [navigate]);
+
+    return (
+        <ThemeCustomization>
+            <ScrollTop>
+                <Routes />
+            </ScrollTop>
+        </ThemeCustomization>
+    );
+};
+
+export default App;
